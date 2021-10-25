64cc134943742eb12a4844653b74739ce807990f
JoseAtovarC <86726784+JoseAtovarC@users.noreply.github.com>
2021-10-25 17:35:43 +0200

    change

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,12 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
-import {Repository} from 'typeorm'
-import { TasksService } from './tasks/tasks.service';
-import { TasksController } from './tasks/tasks.controller';
+
 import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { TaskPgModule } from './task-pg/task-pg.module';
-import * as ormcofig from '../ormconfig'
 
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb+srv://toto:20445718@cluster0.ze4te.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'),
-  TypeOrmModule.forRoot(ormcofig),
-    TasksModule,
-    TaskPgModule],
+TasksModule,
+],
 })
 export class AppModule {}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Body, Post, Get, Param, Put, Delete, HttpCode, HttpStatus } from '@nestjs/common';
 import {TasksService} from './tasks.service'
+import { CreateTaskDTO } from './dto/task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -14,5 +15,26 @@ export class TasksController {
         return resp;
     }
 
+    @Post()
 
+ public async createTask(@Body() createTaskRequest: CreateTaskDTO) {
+        const resp = await this.taskService.createTask(createTaskRequest);
+
+        return resp;
+    }
+
+    @Delete(':id')
+
+ public async deleteTask(@Param('id') id: string) {
+        const resp = await this.taskService.deleteTask(id)
+
+        return resp;
+    }
+
+    @Put('/:id')
+    async updateProduct( @Body() taskDto: CreateTaskDTO, @Param('id') id:string) {
+        const resp=await this.taskService.updateTask(id, taskDto);
+       
+        return resp
+    }
 }
